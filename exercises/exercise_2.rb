87667e25cb4defbbe3a356b077f82711ba5734da
TedP <ted.pampilon@gmail.com>
2021-04-27 01:22:15 +0000

    add excercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+p @store1
+
+@store2 = Store.find_by(id: 2)
+p @store2
+
+
+@store1.update(name: "Calgastore")
+
+p @store1
